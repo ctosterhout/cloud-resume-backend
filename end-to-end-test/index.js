6151a0b9f9681a9da753ce9b0cc00801d46f8bd4
ctosterhout <ctosterhout@gmail.com>
2021-10-24 09:33:56 +0800

    Update index.js

--- end-to-end-test/index.js
+++ end-to-end-test/index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const { DOMAIN_NAME } = require('../config.json');
 
-const sleep = async (r) => await new Promise(r => setTimeout(r, 2000));
+const sleep = async (r) => await new Promise(r => setTimeout(r, 100));
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -10,8 +10,8 @@ const sleep = async (r) => await new Promise(r => setTimeout(r, 2000));
     console.log(`Loading: ${website}`)
     await page.goto(website)
     console.log(`Waiting for API calls to be made`)
-    await sleep(2000)
-    const element = await page.$("#replaceme")
+    await sleep(100)
+    const element = await page.$("#visitors")
     const property = await element.getProperty('innerHTML');
     const count = await property.jsonValue();
     console.log(`Getting page element, count: ${count}`)
